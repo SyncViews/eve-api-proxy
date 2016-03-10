1bb54e9317606a240468fd4d2112a3936f35ce62
William Newbery <wnewbery@hotmail.co.uk>
2016-03-10 17:27:27 +0000

    Reduced logging for CREST request limit

--- libeve-api-proxy/source/CrestConnectionPool.cpp
+++ libeve-api-proxy/source/CrestConnectionPool.cpp
@@ -50,27 +50,21 @@ void CrestConnectionPool::queue(CrestHttpRequest *request)
 
 void CrestConnectionPool::wait_for_request_allowance()
 {
+    //atomic decrement, no need to lock if not managing the reset
     if (--request_allowance >= 0) return;
-
-    std::unique_lock<std::mutex> lock(mutex);
     //only one thread actively waits to reset the count, the others will block on the mutex and
-    //then continue using this second atomic int decrement
+    //then continue using this second int decrement
+    std::unique_lock<std::mutex> lock(mutex);
     if (--request_allowance >= 0) return;
 
-
+    //Make the thread sleep until the second after the previous allowance timestamp
     using std::chrono::steady_clock;
     using std::chrono::seconds;
     auto now = steady_clock::now();
     auto nows = std::chrono::duration_cast<seconds>(now.time_since_epoch());
-
-    log_info() << "CREST request allowance used. Renewing. "
-        << " allowance_time: " << request_allowance_time.count()
-        << " now: " << nows.count()
-        << std::endl;
-
     
     if (request_allowance_time < nows)
-    {
+    {   // More than a second passed since the last allowance anyway
         request_allowance_time = nows;
     }
     else
@@ -79,9 +73,9 @@ void CrestConnectionPool::wait_for_request_allowance()
         std::this_thread::sleep_until(steady_clock::time_point(request_allowance_time));
 
         nows = std::chrono::duration_cast<seconds>(steady_clock::now().time_since_epoch());
-        log_info() << "CREST request allowance renewed at " << nows.count() << std::endl;
     }
     request_allowance = PCREST_MAX_REQS_PER_SEC - 1;
+    log_info() << "CREST request allowance renewed at " << nows.count() << std::endl;
 }
 
 
